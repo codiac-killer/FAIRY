3bccb2406a93c867b03b138b021fbe09217d4336
B.B <bmpisketzhs@gmail.com>
2021-02-17 21:02:03 +0200

    try

---
+++ init_params.h
@@ -0,0 +1,7 @@
+// GRID PARAMETERS
+static const int        ndim = 1;
+static const int        idim = 30, jdim = 30, kdim = 30;
+static const int        g_cells = 2, amr_cells_i = 6, amr_cells_j = 6, amr_cells_k = 6;
+static const int        i_min = 1, i_max = 2;
+static const int        j_min = 1, j_max = 2;
+static const int        k_min = 1, k_max = 2;
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <stdio.h>
+#include "init_params.h"
+
+int main(int argc, char const *argv[])
+{
+	std::cout<<idim<<jdim<<"\n"<<std::endl;
+	return 0;
+}
