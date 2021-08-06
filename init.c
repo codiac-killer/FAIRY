78ef7dbc35477f4f4313bcf1a98a8bc2afc8ff5b
B.B <bmpisketzhs@gmail.com>
2021-08-06 12:09:20 +0300

    init.c now compiles normally

--- init.c
+++ init.c
@@ -16,7 +16,7 @@ int main(int argc, char const *argv[])
 {
 
 
-double x[2*idim+1+dim_b]; // +2 due to ghost zones , +1 due to i/2
+double x[2*idim+1+2*dim_b]; // +2 due to ghost zones , +1 due to i/2
 double z[0]; 
 double y[0];
 if (space == "linear"){
@@ -24,7 +24,7 @@ if (space == "linear"){
 
 
 			double d_i = (i_max - i_min)/idim ;//  dX of numerical grid
-			for (int i = 0; i < 2*idim+4; ++i) // + 2 cells to check from ghost zones
+			for (int i = 0; i < 2*idim+dim_b+1; ++i) // + 2 cells to check from ghost zones
 			{
 				x[i] = (i_min - d_i/2)+i*d_i/2;
 			}
@@ -34,28 +34,27 @@ if (space == "linear"){
 
 
 		}
-	
-}
+
+} 
 
 
-
-
+int p;
 // Scanning the grid for Initial Conditions. main_grid the ndvector from "grid.h"
-for (int i = 0; i < main_grid.i; ++i)
+for (int i = 2; i < main_grid.i; ++i)
 {
 	for (int j = 0; j < main_grid.j; ++j)
 	{
 		for (int k = 0; k < main_grid.k; ++k)
 		{
-			int p = i*main_grid.j*main_grid.k+j*main_grid.k+k; // fix the pointer to the "array"
+			//p = i*main_grid.j*main_grid.k+j*main_grid.k+k; // fix the pointer to the "array"
 
 			// x,y,z from grid.h used as reference to the real numerical distance rather than cell ID
-			main_grid.array[p].p_th = x[i]; 
-			main_grid.array[p].rho  = y[j];
+			//main_grid.array[p].p_th = x[i]; 
+			//main_grid.array[p].rho  = y[j];
 
 			for (int ii = 0; ii < ndim; ++ii)
 			{
-				main_grid.array[p].u[ii]  = x[i]/(x[i]+z[k]);
+				//main_grid.array[p].u[ii]  = x[i]/(x[i]+z[k]);
 			}
 		}
 	}
