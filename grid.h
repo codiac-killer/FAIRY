5e565aa01b98a700feb642eb51554e949ec750c0
B.B <bmpisketzhs@gmail.com>
2021-09-04 17:34:27 +0300

    new

--- grid.h
+++ grid.h
@@ -7,6 +7,10 @@
 #include <functional>
 
 
+
+
+
+
 class primitive {
 public:
 // orizoume to u pinaka me ta primitves 
@@ -23,10 +27,57 @@ public:
 	}
 };
 
+class conservable {
+public:
+// orizoume to u pinaka me ta primitves 
+	float rho_, e_;
+	float p_[n_comp];
+	
+	
+	conservable(){
+		// default times se periptosi pou dn dosei o user 
+		rho_ = 1;
+		e_  = 10;
+		for (int i = 0; i < n_comp; ++i)
+		{
+			p_[i]=0;
+		}
+	}
+	int build(primitive input_){
+		double w_;
+		double lor =0;
+		//First add the u**2 part of the lorentz factor.
+		for (int i = 0; i < n_comp; ++i)
+		{
+			lor = lor + input_.u[i]*input_.u[i];
+		}
+		//now we make the correct lorentz factor
+		lor = pow(1/(1-lor),0.5);
+		
+		//definition of w
+		w_= input_.rho+gamma_par*input_.p_th;
+
+
+		// Now values of conserved ( eq. (5) of delzana_HD )
+		rho_ = input_.rho*lor;
+		e_	 = w_*lor*lor-input_.p_th;
+
+
+		for (int i = 0; i < n_comp; ++i)
+		{
+			p_[i]= w_*lor*lor*input_.u[i];
+		}
+
+
+		return 0;
+	}
+};
+
 std::ostream& operator<<(std::ostream& os, const primitive& pr){
-    os << pr.p_th << ',' << pr.rho;
+    os << pr.p_th << ',' << pr.rho << ',' << pr.u[0] << ',' << pr.u[1] << ',' << pr.u[2] << ','<< (1/(1-pow(pr.u[0],2)-pow(pr.u[1],2)-pow(pr.u[2],2)));
     return os;
 }
+
 
 
 // kanoume klassi gia to grid
@@ -35,12 +86,14 @@ public:
 	// ta int einai oi diastasis
 	int i,j,k;
 	std::vector<primitive> array;
+	std::vector<conservable> conservables_;
 	// apaititai mono i 1i diastasi, ta alla mpainoun 1
 	ndvector(int ii, int jj=1, int kk=1){
 		i = ii;
 		j = jj;
 		k = kk;
 		array.resize(i*j*k);
+		conservables_.resize(i*j*k);
 	}
 	// me tin print tiponoume ta incules tou primitive
 	void print(){
@@ -57,4 +110,7 @@ public:
 };
 
 
-ndvector main_grid(2*idim+1+2*dim_b);
+
+
+// dim_b 
+ndvector main_grid(idim+2*dim_b, (ndim>1) ? jdim : 1, (ndim>2) ? kdim : 1);
